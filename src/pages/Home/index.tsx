f76e4976bde570f965cb319c52c8b29ad6e18498
Prashant Singh <prashantsingh@sharechat.co>
2023-06-25 01:05:14 +0530

    testing GA

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 export default function Home() {
-  const a = 0;
   return (
     <div className="flex justify-center items-center font-semibold text-purple-600 text-4xl h-screen">
       <span className="animate-fire-animation">🔥</span> Aag kisne laga di
